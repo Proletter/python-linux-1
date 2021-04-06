f3b5c3a86cfec146bd977a77baaf2e545e027004
Oluwaseun <Oluseun.ismaila@gmail.com>
2021-04-06 09:40:02 +0000

    commited

--- app.py
+++ app.py
@@ -5,11 +5,6 @@ app = Flask(__name__)
 app.config['APPINSIGHTS_INSTRUMENTATIONKEY'] = '413d7290-841f-4f30-96ac-9353c368cef1'
 appinsights = AppInsights(app)
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 932a223758fbb219b1da9f589a181e43ff4b919c
-
 @app.route('/')
 def hello_world():
     app.logger.info('Info log entry')
